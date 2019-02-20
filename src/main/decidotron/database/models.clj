22c7a9643b41d550371726042206a4114b309079
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-02-20 17:06:01 +0100

    Add query for pro/con Statements

--- src/main/decidotron/database/models.clj
+++ src/main/decidotron/database/models.clj
@@ -24,3 +24,27 @@
   (k/exec-raw [positions-query [issue-slug]] :results))
 
 (comment (positions-for-issue "was-sollen-wir-mit-20-000eur-anfangen"))
+
+(defn pro-con-for-positions [position-ids]
+  (k/select ["textversions" :tv]
+    (k/fields [:statements.stmt_uid :position_uid] :statements.uid [:tv.content :text] :statements.is_supportive)
+    (k/join :inner
+      [(k/subselect "premises"
+         (k/fields [:statement_uid :uid] :arguments.is_supportive :arguments.stmt_uid)
+         (k/join :inner
+           [(k/subselect "arguments"
+              (k/fields :premisegroup_uid :is_supportive [:conclusion_uid :stmt_uid])
+              (k/where (and
+                         (in :conclusion_uid (vec position-ids))
+                         (= :is_disabled false))))
+            :arguments]
+           (= :arguments.premisegroup_uid :premises.premisegroup_uid)))
+       :statements]
+      (= :statements.uid :tv.statement_uid))))
+
+(defn pro-con-for-position [position-id]
+  (pro-con-for-positions [position-id]))
+
+(comment
+  (pro-con-for-position 85)
+  (pro-con-for-positions [83, 85]))
