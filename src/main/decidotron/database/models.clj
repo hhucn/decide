df96dd954c9c13176bf9435b52cae88c26c69c57
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-02-21 16:05:11 +0100

    Use korma SQL for queries

--- src/main/decidotron/database/models.clj
+++ src/main/decidotron/database/models.clj
@@ -1,29 +1,67 @@
 (ns decidotron.database.models
   (:require [korma.core :as k]))
 
-(def positions-query
-  "
-  SELECT statements.uid as id, dpc.cost, tv.content as text
-  FROM (SELECT issue_uid, statements.uid
-         FROM statements
-         JOIN (SELECT statement_uid, issue_uid
-                FROM issues
-                JOIN statement_to_issue s on issues.uid = s.issue_uid
-                WHERE issues.slug = ?) as sti
-         on statements.uid = sti.statement_uid
-         WHERE is_position
-         and not is_disabled) statements
-  JOIN decidotron_position_cost as dpc on statements.uid = dpc.position_id
-  JOIN textversions as tv on statements.uid = tv.statement_uid;
-  ")
+
+(declare cost textversion issue statement position)
+
+(k/defentity cost
+  (k/pk :position_id)
+  (k/table :decidotron_position_cost)
+  (k/entity-fields :cost))
+
+(k/defentity textversion
+  (k/pk :uid)
+  (k/table :textversions)
+  (k/entity-fields :content))
+
+(k/defentity issue
+  (k/pk :uid)
+  (k/table :issues)
+  (k/many-to-many statement :statement_to_issue {:lfk :statement_to_issue.issue_uid :rfk :statement_to_issue.statement_uid}))
+
+(k/defentity statement
+  (k/pk :uid)
+  (k/table :statements)
+  (k/has-one cost {:fk :position_id})
+  (k/has-one textversion {:fk :statement_uid})
+  (k/many-to-many issue :statement_to_issue {:rfk :statement_to_issue.issue_uid
+                                             :lfk :statement_to_issue.statement_uid}))
+
+(k/defentity position
+  (k/pk :uid)
+  (k/table (k/raw "(SELECT * FROM statements WHERE statements.is_position = true)") :position)
+  (k/has-one cost {:fk :position_id})
+  (k/has-one textversion {:fk :statement_uid})
+  (k/many-to-many issue :statement_to_issue {:rfk :statement_to_issue.issue_uid
+                                             :lfk :statement_to_issue.statement_uid}))
+
+(defn positions-by-ids [ids]
+  (for [{:keys [uid content cost]}
+        (k/select statement
+          (k/where (and (in :uid ids) (= :is_position true)))
+          (k/with cost)
+          (k/with textversion))]
+    #:dbas.position{:id uid
+                    :text content
+                    :cost cost}))
+
+(defn position-by-id [id]
+  (first (positions-by-ids [id])))
+
+(defn positions-for-issue [slug]
+  (for [{:keys [uid cost content]}
+        (:statements (first (k/select issue
+                              (k/with statement
+                                (k/with textversion)
+                                (k/with cost)
+                                (k/where (and
+                                           (= :is_position true)
+                                           (= :is_disabled false))))
+                              (k/where (and (= :slug slug))))))]
+     #:dbas.position{:id uid :cost cost :text content}))
 
 (defn index-by [l k]
   (apply hash-map (mapcat #(vector (k %) %) l)))
-
-(defn positions-for-issue [issue-slug]
-  (k/exec-raw [positions-query [issue-slug]] :results))
-
-(comment (positions-for-issue "was-sollen-wir-mit-20-000eur-anfangen"))
 
 (defn pro-con-for-positions [position-ids]
   (k/select ["textversions" :tv]
