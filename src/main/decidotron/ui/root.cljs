ef57c019a622dcd3bffdb568e4b8a72c72726071
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-04-08 21:09:41 +0200

    Add reload button to failed state of router

--- src/main/decidotron/ui/root.cljs
+++ src/main/decidotron/ui/root.cljs
@@ -28,31 +28,30 @@
 
 (defn main-page [this]
   (dom/div
-   (dom/h1 "Verteilung von Qualitätsverbesserungsmitteln in der Informatik")
-   (dom/p :.lead
-          "Die WE Informatik möchte Ihnen die Möglichkeit geben selbst über
-          Qualitätsverbesserungsmittel und deren Verwendung abzustimmen. Daher
-          haben Sie hier die Möglichkeit Vorschläge einzubringen, die von der
-          Studierendenschaft der Informatik später zur Abstimmung freigegeben
-          werden.")
+    (dom/h1 "Verteilung von Qualitätsverbesserungsmitteln in der Informatik")
+    (dom/p :.lead
+      "Die WE Informatik möchte Ihnen die Möglichkeit geben selbst über
+     Qualitätsverbesserungsmittel und deren Verwendung abzustimmen. Daher
+     haben Sie hier die Möglichkeit Vorschläge einzubringen, die von der
+     Studierendenschaft der Informatik später zur Abstimmung freigegeben
+     werden.")
 
-   (dom/p
-    "Wichtig dabei ist, dass die Vorschläge alle der Qualitätsverbesserung
-    dienen, denn die dafür aufgebrachten Mittel müssen zweckgebunden verwendet
-    werden.")
+    (dom/p
+      "Wichtig dabei ist, dass die Vorschläge alle der Qualitätsverbesserung
+     dienen, denn die dafür aufgebrachten Mittel müssen zweckgebunden verwendet
+     werden.")
 
-   (dom/p
-    "Diese Diskussion ist Teil einer wissenschaftlichen Studie. Wir möchten
-    hierbei unter Anderem unsere Softwaresysteme testen. Es handelt sich hierbei
-    um zwei verschiedene Systeme: D-BAS zum Diskutieren und decide um
-    anschließend die Vorschläge zu Priorisieren. Wir beginnen zunächst mit der
-    Diskussion und schalten in der zweiten Woche die Priorisierung frei.")
+    (dom/p
+      "Diese Diskussion ist Teil einer wissenschaftlichen Studie. Wir möchten
+     hierbei unter Anderem unsere Softwaresysteme testen. Es handelt sich hierbei
+     um zwei verschiedene Systeme: D-BAS zum Diskutieren und decide um
+     anschließend die Vorschläge zu Priorisieren. Wir beginnen zunächst mit der
+     Diskussion und schalten in der zweiten Woche die Priorisierung frei.")
 
-   (dom/br)
-   (dom/p :.text-center
-          (dom/a :.btn.btn-primary
-            {:href (str js/dbas_host "/discuss/" routing/hardcoded-slug)}
-            "Hier geht's zur Diskussion!"))))
+    (dom/br)
+    (dom/p :.text-center
+      (dom/a :.btn.btn-primary {:href (str js/dbas_host "/discuss/" routing/hardcoded-slug)}
+       "Hier geht's zur Diskussion!"))))
 
 (defsc-route-target MainPage [this _]
   {:query           []
@@ -68,7 +67,7 @@
   (case current-state
     :initial (main-page this)
     :pending (dom/div "Loading...")
-    :failed (dom/div "Oops")
+    :failed (dom/div "Oops" (dom/a {:onClick #(js/location.reload) :target "_self"} "Neu laden!"))
     (main-page this)))
 
 (def ui-router (prim/factory RootRouter))
