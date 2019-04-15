61f6f464410ac6f4c99cec01fcac58d23fb92e1f
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-04-15 13:49:16 +0200

    Minor sprachliche Changes

--- src/main/decidotron/ui/static_pages/algorithm.cljs
+++ src/main/decidotron/ui/static_pages/algorithm.cljs
@@ -12,18 +12,18 @@
    :will-enter      (fn [_ _] (dr/route-immediate [:screens/id :algorithm-screen]))
    :will-leave      (fn [_] true)}
   (dom/div :#doc
-    (dom/h1 "Erklärung des Experiment-Ablaufes")
+    (dom/h1 "Erklärung des Ablaufes")
     (dom/h2 "Vorschlags-Phase")
     (dom/p "In der ersten Phase ist es möglich über unser Argumentationssystem "
       (dom/em "D-BAS")
       " eigene Vorschläge und eine grobe Preisabschätzung für den Vorschlag abzugeben. Die Vorschläge können dann ausdiskutiert werden. Dabei wird Sie D-BAS durch einen Dialog führen und Ihre Meinungen zum eigenen Vorschlag sowie zu den anderen Vorschlägen einholen. Sie können jederzeit die Meinungen und Vorschläge der anderen Teilnehmenden einsehen und gegen/für diese Meinungen argumentieren. Sollte jemand auf eine Ihrer Aussagen Bezug nehmen, so werden Sie per Mail benachrichtigt. So können Sie direkt wieder in die Diskussion mit einsteigen und Ihre Aussagen verteidigen.")
     (dom/p "Die erste Phase dauert eine Woche. In der zweiten Woche können Sie weiter auf D-BAS diskutieren, Sie sollten sich aber bewusst sein, dass die Abstimmungs-Phase dann schon begonnen hat.")
     (dom/h2 "Abstimmungs-Phase")
-    (dom/p "Für die Abstimmungsphase wechseln wir in unser Entscheidungsfindungssystem "
+    (dom/p "Für die Abstimmungs-Phase wechseln wir in unser Entscheidungsfindungssystem "
       (dom/em "decide") ". Schauen Sie sich dort nun die Vorschläge an und entscheiden Sie, von welchen Sie möchten, dass sie umgesetzt werden. Wenn Sie die Vorschläge nicht gut finden, dann wählen Sie diese einfach nicht aus.")
     (dom/p "Nachdem Sie eine Auswahl getätigt haben, sollten Sie die Vorschläge priorisieren. Klicken Sie die wichtigsten Vorhaben an die Spitze der Liste und die weniger wichtigen an das Ende.")
     (dom/p "Das endgültige Ergebnis an umzusetzenden Vorschlägen wird ausgewählt, indem jeder Priorität ein Wert zugeordnet wird. Neben Ihnen haben noch viele andere ihre Stimmen abgegeben und ihre Stimmen bekommt auch einen Wert. Jede erste Priorität bekommt denselben Wert, jede zweite Priorität denselben kleineren Wert, usw.")
-    (dom/p "Anschließend werden die Werte aller Vorschläge miteinander addiert, um somit eine Rangfolge zu erhalten. In dem Ergebnis werden nun die am besten bewerteten Vorschläge eingebracht, solange sie mit in das restliche Budget passen.")
+    (dom/p "Anschließend werden die Werte aller Vorschläge summiert, um somit eine Rangfolge zu erhalten. In dem Ergebnis werden nun die am besten bewerteten Vorschläge eingebracht, solange sie mit in das restliche Budget passen.")
     (dom/p "Diese Phase dauert eine Woche und findet direkt im Anschluss an die Vorschlags-Phase statt. Im Anschluss finden Sie die Ergebnisse unter "
       (dom/a
         {:href   "https://decide.dbas.cs.uni-duesseldorf.de/preferences/verteilung-von-qualitatsverbesserungsmittel-in-der-informatik/result",
