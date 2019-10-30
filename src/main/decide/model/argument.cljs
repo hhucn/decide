c03e4252d5d70f7f4bc5554258ac2ce1bcfd9888
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-10-30 18:26:34 +0100

    Add UI for new arguments

--- src/main/decide/model/argument.cljs
+++ src/main/decide/model/argument.cljs
@@ -1,12 +1,13 @@
 (ns decide.model.argument
-  (:require [com.fulcrologic.fulcro.dom :as dom :refer [div]]
+  (:require [com.fulcrologic.fulcro.dom :as dom :refer [div button p a form option label input span]]
             [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
             [com.fulcrologic.fulcro-css.css :as css]
             [com.fulcrologic.fulcro.algorithms.form-state :as fs]
             [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
             [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]
             [ghostwheel.core :as g :refer [>defn => | ?]]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [com.fulcrologic.fulcro.dom.events :as evt]))
 
 (s/def :argument/id int?)
 (s/def :argument/text string?)
@@ -16,6 +17,72 @@
 (s/def ::argument (s/keys :req-un [:argument/id]
                     :opt-un [:argument/text :argument/type
                              :argument/pros :argument/cons]))
+
+(defn select [comp attribute options current-value]
+  (dom/select :.form-control
+    {:onChange #(m/set-value! comp attribute (keyword (evt/target-value %)))
+     :value current-value}
+    (for [[type label] options
+          :let [value (name type)]]
+      (option {:key type
+               :value value}
+        label))))
+
+(defmutation add-argument [{:keys [argument subtype]}]
+  (action [env] true))
+
+(defsc NewArgumentForm [this {:keys [new-argument/id]
+                              :ui/keys [open? new-argument new-subtype pro?]}]
+  {:query [:new-argument/id
+           :ui/open?
+           :ui/new-argument :ui/new-subtype :ui/pro?
+           fs/form-config-join]
+   :ident :new-argument/id
+   :form-fields #{:ui/new-argument :ui/new-subtype}
+   :initial-state (fn [{:keys [argument/id pro?] :or {pro? true}}]
+                    {:new-argument/id id
+                     :ui/new-argument ""
+                     :ui/pro? pro?
+                     :ui/open? false
+                     :ui/new-subtype :undermine})}
+  (div :.collapse
+    {:classes [(when open? "show")]}
+    (form :.container.border
+      {:onSubmit #(fn submit [e]
+                    (evt/prevent-default! e)
+                    #_(comp/transact! this [(add-argument {:argument new-argument
+                                                           :subtype :subtype})]))}
+      (div :.form-group
+        (label "Dein neues Argument "
+          (if pro?
+            (a :.text-success {:onClick #(m/toggle! this :ui/pro?)} "dafür")
+            (a :.text-danger {:onClick #(m/toggle! this :ui/pro?)} "dagegen"))
+          ":")
+        (input :.form-control
+          {:type "text" :value new-argument
+           :onChange #(m/set-string! this :ui/new-argument :event %)}))
+
+      (div :.form-group
+        (label "Wieso nennst du dieses Argument?")
+        (select this
+          :ui/new-subtype
+          {:undermine "Undermine"
+           :undercut "Undercut"}
+          (name new-subtype)))
+
+      (button :.btn.btn-primary
+        {:type "submit"}
+        "Submit"))))
+
+(def ui-new-argument (comp/factory NewArgumentForm))
+
+(defmutation open-add-new-argument [{:keys [argument/id pro?]}]
+  (action [{:keys [state ref]}]
+    (js/console.log ref)
+    (swap! state update-in [:new-argument/id id]
+      #(-> %
+         (assoc :ui/pro? pro?)
+         (assoc :ui/open? true)))))
 
 (defsc Argument [this {:argument/keys [id text]} {:keys [argumentation-root]}]
   {:query [:argument/id :argument/text
@@ -40,7 +107,7 @@
              :flexDirection "column"}}
     children))
 
-(defsc ProCon [this {:argument/keys [pros cons]} computed]
+(defsc ProCon [this {:argument/keys [id pros cons]} computed]
   {:query [:argument/id
            :argument/text
            :argument/type
@@ -52,11 +119,15 @@
                                    argument))}
   (div :.row
     (half-row
-      (dom/h6 :.argumentation-header.bg-success "Pro Argumente")
+      (dom/h6 :.argumentation-header.bg-success "Pro Argumente"
+        (button {:onClick #(comp/transact! this [(open-add-new-argument {:argument/id id
+                                                                         :pro? true})])} "+"))
       (map #(ui-argument (comp/computed % computed)) pros))
 
     (half-row
-      (dom/h6 :.argumentation-header.bg-danger "Con Argumente")
+      (dom/h6 :.argumentation-header.bg-danger "Con Argumente"
+        (button {:onClick #(comp/transact! this [(open-add-new-argument {:argument/id id
+                                                                         :pro? false})])} "+"))
       (map #(ui-argument (comp/computed % computed)) cons))))
 
 (def ui-procon (comp/factory ProCon {:keyfn :argument/id}))
@@ -94,15 +165,17 @@
 
 (def ui-upstream-item (comp/factory UpstreamItem))
 
-(defsc Argumentation [this {:argumentation/keys [upstream current-argument]}]
+(defsc Argumentation [this {:argumentation/keys [upstream current-argument new-argument]}]
   {:query [:argument/id
            {:argumentation/upstream (comp/get-query UpstreamItem)}
-           {:argumentation/current-argument (comp/get-query ProCon)}]
+           {:argumentation/current-argument (comp/get-query ProCon)}
+           {:argumentation/new-argument (comp/get-query NewArgumentForm)}]
    :ident :argument/id
    :initial-state (fn [{:argument/keys [id] :as root-arg}]
                     {:argument/id id
+                     :argumentation/new-argument (comp/initial-state NewArgumentForm {:argument/id id})
                      :argumentation/upstream []
-                     :argumentation/current-argument (comp/initial-state ProCon {:argument/id id})})}
+                     :argumentation/current-argument (comp/initial-state ProCon (:argumentation/current-argument root-arg))})}
 
   (div
     (dom/ol :.list-group
@@ -112,6 +185,7 @@
             (comp/computed props
               {:jmp-fn #(comp/transact! this [(jump-backwards {:position i})])})))
         (concat upstream [current-argument])))
+    (ui-new-argument new-argument)
     (ui-procon
       (comp/computed current-argument
         {:argumentation-root this}))))
--- src/workspaces/decide/argumentation_ws.cljs
+++ src/workspaces/decide/argumentation_ws.cljs
@@ -26,21 +26,21 @@
   (ct.fulcro/fulcro-card
     {::ct.fulcro/root arg/Argumentation
      ::ct.fulcro/initial-state
-           (fn [] #:argumentation{:argument/id 1
-                                  :upstream []
-                                  :current-argument #:argument{:id 1
-                                                               :text "Es soll ein 3D Drucker angeschafft werden."
-                                                               :type :position
-                                                               :pros [#:argument{:id 42
-                                                                                 :text "3d Drucker sind praktisch"
-                                                                                 :pros [#:argument{:id 142 :text "Weil sie praktisch sind." :type :pro}]
-                                                                                 :type :pro}
-                                                                      #:argument{:id 421
-                                                                                 :text "3d Drucker sind sehr praktisch"
-                                                                                 :type :pro}]
-                                                               :cons [#:argument{:id 43
-                                                                                 :text "3d Drucker sind teuer"
-                                                                                 :type :con}]}})}))
+           #:argumentation{:argument/id 1
+                           :upstream []
+                           :current-argument #:argument{:id 1
+                                                        :text "Es soll ein 3D Drucker angeschafft werden."
+                                                        :type :position
+                                                        :pros [#:argument{:id 42
+                                                                          :text "3d Drucker sind praktisch"
+                                                                          :pros [#:argument{:id 142 :text "Weil sie praktisch sind." :type :pro}]
+                                                                          :type :pro}
+                                                               #:argument{:id 421
+                                                                          :text "3d Drucker sind sehr praktisch"
+                                                                          :type :pro}]
+                                                        :cons [#:argument{:id 43
+                                                                          :text "3d Drucker sind teuer"
+                                                                          :type :con}]}}}))
 
 (ws/defcard Argument
   {::wsm/card-width  2 ::wsm/card-height 5}
@@ -49,3 +49,8 @@
      ::ct.fulcro/initial-state
            (fn [] #:argument{:id 42
                              :text "3d Drucker sind praktisch"})}))
+
+(ws/defcard NewArgumentForm
+  (ct.fulcro/fulcro-card
+    {::ct.fulcro/root arg/NewArgumentForm
+     ::ct.fulcro/initial-state {:argument/id 420}}))
