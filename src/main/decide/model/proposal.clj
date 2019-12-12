8408a63bafb41a035ba578465fe7d91b7f526388
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-12-12 13:50:25 +0100

    Add spec for transaction report

--- src/main/decide/model/proposal.clj
+++ src/main/decide/model/proposal.clj
@@ -1,7 +1,7 @@
 (ns decide.model.proposal
   (:require
     [datahike.api :as d]
-    [datahike.core :refer [squuid db? conn?]]
+    [datahike.core :refer [squuid db? conn? datom?]]
     [com.fulcrologic.guardrails.core :as g :refer [>defn => | ?]]
     [com.wsscode.pathom.core :as p]
     [com.wsscode.pathom.connect :as pc :refer [defresolver defmutation]]
@@ -20,9 +20,20 @@
 
 (s/def :vote/utility int?)
 
+(s/def :datomic.tx-report/db-before db?)
+(s/def :datomic.tx-report/db-after db?)
+(s/def :datomic.tx-report/tx-data (s/coll-of datom?))
+(s/def :datomic.tx-report/tempids map?)
+(s/def :datomic.tx-report/tx-meta any?)
+(s/def ::d/tx-report (s/keys :req-un [:datomic.tx-report/db-before ; db value before transaction
+                                      :datomic.tx-report/db-after ; db value after transaction
+                                      :datomic.tx-report/tx-data ; plain datoms that were added/retracted from db-before
+                                      :datomic.tx-report/tempids ; map of tempid from tx-data => assigned entid in db-after
+                                      :datomic.tx-report/tx-meta]))
+
 (>defn add-proposal! [conn {:proposal/keys [id cost details]
                             :argument/keys [text author created-when]}]
-  [conn? any? => map?]
+  [conn? any? => ::d/tx-report]
   (d/transact conn
     [(merge
        #:proposal{:db/id       "new-proposal"
@@ -35,34 +46,33 @@
                   :author       author
                   :created-when (str created-when)})]))
 
-(defmutation new-proposal [{:keys [connection AUTH/account-id] :as env} {:proposal/keys [id cost details]
-                                                                         :argument/keys [text] :as params}]
+(defmutation new-proposal [{:keys [connection AUTH/account-id] :as env}
+                           {:proposal/keys [id cost details]
+                            :argument/keys [text]}]
   {::pc/params [:proposal/id :argument/text :proposal/cost :proposal/details]
    ::pc/output [:proposal/id]
    ::s/params  ::new-proposal}
   (when account-id
-    (if (s/valid? ::new-proposal params)
-      (let [real-id   (squuid)
-            tx-report (add-proposal! connection
-                        {:proposal/id           real-id
-                         :proposal/cost         (Long/parseLong cost)
-                         :proposal/details      details
-                         :argument/text         text
-                         :argument/author       [:account/id account-id]
-                         :argument/created-when (Instant/now)})]
-        (log/debug "New UUID for proposal" real-id)
-        {:proposal/id real-id
-         ::p/env      (assoc env :db (:db-after tx-report))
-         :tempids     {id real-id}})
-      (log/spy :error (s/explain ::new-proposal params)))))
+    (let [real-id   (squuid)
+          tx-report (add-proposal! connection
+                      {:proposal/id           real-id
+                       :proposal/cost         (Long/parseLong cost)
+                       :proposal/details      details
+                       :argument/text         text
+                       :argument/author       [:account/id account-id]
+                       :argument/created-when (Instant/now)})]
+      (log/debug "New UUID for proposal" real-id)
+      {:proposal/id real-id
+       ::p/env      (assoc env :db (:db-after tx-report))
+       :tempids     {id real-id}})))
 
 
 (s/def ::upsert-vote (s/keys :req [:proposal/id :account/id :vote/utility]))
 
-(defn upsert-vote [conn {proposal-id :proposal/id
-                         account     :account/id
-                         utility     :vote/utility}]
-  [conn? ::upsert-vote => map?]
+(>defn upsert-vote! [conn {proposal-id :proposal/id
+                           account     :account/id
+                           utility     :vote/utility}]
+  [conn? ::upsert-vote => ::d/tx-report]
   (d/transact conn
     [{:db/id                 "vote"
       :vote/account+proposal (str account "+" proposal-id)
@@ -71,17 +81,19 @@
      [:db/add [:account/id account] :account/votes "vote"]]))
 
 
-(defmutation set-vote [{:keys [connection AUTH/account-id] :as env} {proposal-id :proposal/id
-                                                                     account     :account/id :as params}]
+(defmutation set-vote [{:keys [connection AUTH/account-id] :as env}
+                       {proposal-id :proposal/id
+                        account     :account/id :as params}]
   {::pc/params [:proposal/id :account/id :account/id]
    ::pc/output [:proposal/id]
    ::s/params  ::upsert-vote}
-  (when (and account-id (= account-id account))
-    (let [{:keys [db-after]} (upsert-vote connection params)]
+  (if (and account-id (= account-id account))
+    (let [{:keys [db-after]} (upsert-vote! connection params)]
       {:proposal/id proposal-id
-       ::p/env      (assoc env :db db-after)})))
+       ::p/env      (assoc env :db db-after)})
+    (throw (ex-info "Authorization failed" {:account/id account-id}))))
 
-(defresolver resolve-votes [{:keys [db AUTH/account-id] :as env} {proposal :proposal/id}]
+(defresolver resolve-votes [{:keys [db AUTH/account-id]} {proposal :proposal/id}]
   {::pc/input  #{:proposal/id}
    ::pc/output [:vote/utility]}
   (if account-id
