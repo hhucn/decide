6119b0dd1e4acb8341ef2f3cd292f29513eab7bb
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-12-20 14:57:22 +0100

    Fix issues with deps resolving when building for production

--- project.clj
+++ project.clj
@@ -2,19 +2,20 @@
 
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :lein-tools-deps/config {:config-files    [:install :user :project]
+                           :resolve-aliases [:dev]}
 
   :main decide.server-main
-  :clean-targets ^{:protect false} [:target-path "resources/public/js/" "resources/public/workspaces/js/" "resources/public/css/main.css.map"]
+  :clean-targets ^{:protect false} [:target-path
+                                    "resources/public/js/"
+                                    "resources/public/workspaces/js/"
+                                    "resources/public/css/main.css.map"]
 
+  :aliases {"cljs-release" ["run" "-m" "shadow.cljs.devtools.cli" "release" "main"]}
   :profiles {:uberjar {:main           decide.server-main
                        :aot            [decide.server-main]
                        :uberjar-name   "decide.jar"
 
                        :jar-exclusions [#"public/js/test" #"public/js/workspaces" #"public/workspaces.html"]
                        :prep-tasks     ["clean" ["clean"]
-                                        "compile" ["with-profile" "cljs" "run" "-m" "shadow.cljs.devtools.cli" "release" "main"]]}
-             :cljs    {:source-paths ["src/main"]
-                       :dependencies [[com.google.javascript/closure-compiler-unshaded "v20190325"]
-                                      [org.clojure/google-closure-library "0.0-20190213-2033d5d9"]
-                                      [thheller/shadow-cljs "2.8.83"]]}})
+                                        "compile" ["cljs-release"]]}})
